fe9c19a705e12f184c0743e5b76bda6df6fe8455
sponsar <406665325@qq.com>
2016-02-15 14:00:44 -0800

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.h
+//
+//  Created by : 黄 信尧
+//  Project    : SudokuByYao
+//  Date       : 2/2/16
+//
+//  Copyright (c) 2016 黄 信尧.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+// -----------------------------------------------------------------------
+
+@interface AppDelegate : CCAppDelegate
+
+// -----------------------------------------------------------------------
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
