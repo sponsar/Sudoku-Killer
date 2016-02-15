68ad10dd98430eb8c03e5eca54a0aabfea90d35f
sponsar <406665325@qq.com>
2016-02-15 14:09:34 -0800

    Create Oops.h

---
+++ Oops.h
@@ -0,0 +1,36 @@
+//
+//  Oops.h
+//
+//  Created by : 黄 信尧
+//  Project    : Sudoku
+//  Date       : 2/7/16
+//
+//  Copyright (c) 2016 黄 信尧.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------
+
+@interface Oops : CCNodeColor
+
+// -----------------------------------------------------------------
+// properties
+
+// -----------------------------------------------------------------
+// methods
+
++ (instancetype)node;
+- (instancetype)initWithString:(NSString *)hint;
+
+// -----------------------------------------------------------------
+
+@end
+
+
+
+
