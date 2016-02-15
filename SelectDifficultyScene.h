a99d163299b8c0aa94903e55e0ce6bfec64e3a05
sponsar <406665325@qq.com>
2016-02-15 14:06:52 -0800

    Create SelectDifficultyScene.h

---
+++ SelectDifficultyScene.h
@@ -0,0 +1,37 @@
+//
+//  SelectDifficultyScene.h
+//
+//  Created by : 黄 信尧
+//  Project    : SudokuByYao
+//  Date       : 2/4/16
+//
+//  Copyright (c) 2016 黄 信尧.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "TableViewDataSource.h"
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------
+
+@interface SelectDifficultyScene : CCScene
+
+// -----------------------------------------------------------------
+// properties
+
+// -----------------------------------------------------------------
+// methods
+
++ (instancetype)node;
+- (instancetype)init;
+
+// -----------------------------------------------------------------
+
+@end
+
+
+
+
