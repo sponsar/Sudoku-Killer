7d18a2ec75b150f0393cf7370a205180a0a4e30e
sponsar <406665325@qq.com>
2016-02-15 14:05:57 -0800

    Create SolveBoardScene.h

---
+++ SolveBoardScene.h
@@ -0,0 +1,36 @@
+//
+//  SolveBoardScene.h
+//
+//  Created by : 黄 信尧
+//  Project    : SudokuByYao
+//  Date       : 2/3/16
+//
+//  Copyright (c) 2016 黄 信尧.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------
+
+@interface SolveBoardScene : CCScene
+
+// -----------------------------------------------------------------
+// properties
+
+// -----------------------------------------------------------------
+// methods
+
++ (instancetype)node;
+- (instancetype)init;
+
+// -----------------------------------------------------------------
+
+@end
+
+
+
+
